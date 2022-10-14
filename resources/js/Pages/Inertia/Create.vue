f7a3b181e89a645b12e437e353f54fff7cb0ae88
tenjuhat <tenju.hattori@rakus-partners.co.jp>
2022-10-14 16:28:28 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="submitFunction">
+    <input type="text" name="title" v-model="form.title"><br>
+    <div v-if="errors.title">{{ errors.title }}</div>
+    <input type="text" name="content" v-model="form.content"><br>
+    <div v-if="errors.content">{{ errors.content }}</div>
+    <button>送信</button>
+  </form>
+</template>
+
+<script setup>
+import { Inertia } from '@inertiajs/inertia';
+import { reactive } from 'vue';
+
+defineProps({
+  errors: Object
+})
+
+const form = reactive({
+  title: null,
+  content: null
+})
+
+const submitFunction = () => {
+  Inertia.post('/inertia', form)
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,24 @@
+<template>
+  {{ id }} <br>
+  {{ blog.title }} <br>
+  <button @click="deleteConfirm(blog.id)">削除</button>
+</template>
+
+<script setup>
+import { Inertia } from '@inertiajs/inertia';
+
+defineProps({
+  id: String,
+  blog: Object
+})
+
+const deleteConfirm = id => {
+  Inertia.delete(`/inertia/${id}`, {
+    onBefore: () => confirm('本当に削除しますか')
+  })
+}
+</script>
+
+<style>
+
+</style>
